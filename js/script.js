f51a100445c984a57b79583c669ff56832459c05
Claudio Fuentes <claudiofuentesperez@gmail.com>
2023-06-13 14:19:40 -0400

    agregamos primeros archivos del proyecto

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//h1 
+
+const h1 = document.querySelector('h1');
+const form = document.querySelector('form');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+
+
+//btn.addEventListener ('click', btnOnClick);
+//form.addEventListener ('submit', sumarInputValues);
+
+
+//function btnOnClick(){
+//     function sumarInputValues (event) {
+//    // console.log(event);
+//     event.preventDefault();
+    
+//    const sumaInputs = input1.value + input2.value;
+//     pResult.innerText = "Resultado: " + sumaInputs;
+// }
+
+form.addEventListener('click', sumarInputValues);
+
+function sumarInputValues(event) {
+    const sumaInputs = input1.value + ' ' + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
+
